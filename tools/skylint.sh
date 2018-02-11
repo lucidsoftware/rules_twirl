7fa58ea4551fd1f09b33f79795970e525be47483
James Judd <james@lucidchart.com>
2018-02-10 19:53:47 -0700

    More Travis

--- tools/skylint.sh
+++ tools/skylint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo "Running Skylint"
 skylint_path=src/tools/skylark/java/com/google/devtools/skylark/skylint
 bazel build @io_bazel//${skylint_path}:Skylint \
   && find . -type f -name *.bzl \
