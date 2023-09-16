9fb6bc25dabae74cb3a6f0a036d1a0c61a6c670c
Thayne McCombs <thayne@lucidchart.com>
2023-09-16 00:19:49 -0600

    Fix lint script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Running buildifier"
-bazel run //:buildifier -r --lint=warn "$(bazel info workspace)"
+bazel run //:buildifier -- -r --lint=warn "$(bazel info workspace)"
