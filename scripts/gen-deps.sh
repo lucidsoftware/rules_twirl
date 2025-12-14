995a7236059ff741f5e3c3613c83138c0d2979bf
James Judd <james@lab-y.com>
2025-12-14 16:47:21 -0700

    Update dependencies

--- scripts/gen-deps.sh
+++ scripts/gen-deps.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 cd "$(dirname "$0")"
 
-bazel run @twirl_compiler_cli_3//:pin
-bazel run @twirl_compiler_cli_2_13//:pin
-bazel run @twirl_test_3//:pin
-bazel run @twirl_test_2_13//:pin
+REPIN=1 bazel run @twirl_compiler_cli_3//:pin
+REPIN=1 bazel run @twirl_compiler_cli_2_13//:pin
+REPIN=1 bazel run @twirl_test_3//:pin
+REPIN=1 bazel run @twirl_test_2_13//:pin
--- versions.bzl
+++ versions.bzl
@@ -1,5 +1,5 @@
 # Please ensure these stay up-to-date with the versions in `MODULE.bazel`. Unfortunately,
 # `MODULE.bazel` files can't call `load`, so we have to copy them there.
-scala_2_13_version = "2.13.14"
-scala_3_version = "3.5.1"
-twirl_version = "2.0.7"
+scala_2_13_version = "2.13.18"
+scala_3_version = "3.3.7"
+twirl_version = "2.0.9"
